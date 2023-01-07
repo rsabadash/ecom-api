b11478fed8346cff2d7df7958ada64cdbcf2b466
dKostreba <kostrebadd@gmail.com>
2023-01-07 16:20:37 +0200

    EA-61 Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { API_URI, VERSION_URI } from './common/constants/main.constants';
+import { swaggerConfig } from './common/config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,9 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3001);
 }
