dbf37ca894070c797f00b342240c59113ed137e1
Roman Sabadash <r.v.sabadash@gmail.com>
2023-02-08 19:24:52 +0200

    Update Categories logic

--- src/common/interfaces/dropdown-list.interface.ts
+++ src/common/interfaces/dropdown-list.interface.ts
@@ -1,5 +1,9 @@
 import { ObjectId } from 'mongodb';
 
+export interface DropdownListQueryParams {
+  _id?: ObjectId;
+}
+
 export interface DropdownListItem {
   id: string | ObjectId;
   value: string;
